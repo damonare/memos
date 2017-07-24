e74ac31e9e509b5d4939db6421ce83f9d644fac1
Damonare <jztan1996@gmail.com>
2017-07-24 19:54:41 +0800

    添加webpack插件

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,12 @@
  */
 const path = require('path');
 const webpack = require('webpack');
+const OpenBrowserPlugin = require('open-browser-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const port = 3000;
 
 module.exports = {
-    devtool: 'sourceMap',
+    devtool: '#source-map',
     entry: {
         bundle: './app/main.jsx',
         vendor: ['react', 'react-dom', 'jquery', 'react-router', 'redux'],
@@ -29,7 +32,7 @@ module.exports = {
                 test: /\.less$/,
                 use: [
                     'style-loader',
-                    'css-loader?sourceMap',
+                    'css-loader?#sourceMap',
                     'postcss-loader',
                     'less-loader',
                 ],
@@ -40,11 +43,15 @@ module.exports = {
         new webpack.optimize.CommonsChunkPlugin({
             name: ['vendor'],
         }),
+				new OpenBrowserPlugin({
+		      url: `http://localhost:${port}`
+		    }),
+				new BundleAnalyzerPlugin()
     ],
     devServer: {
         compress: true, // 启用gzip压缩
         contentBase: path.join(__dirname, 'app'),
-        port: 3000, // 运行端口3000
+        port: port, // 运行端口3000
         inline: true,
         hot: true,
         historyApiFallback: true,
